3a90ae06182d5120e6a4b78ee4cf20b292ca5f5f
Guillermo Daniel Toyos Marfurt <guillermo.toyos@fing.edu.uy>
2021-09-23 12:19:51 -0300

    initial commit

---
+++ cliente.py
@@ -0,0 +1,253 @@
+# EL CLIENTE 
+SERVER_HOSTNAME = "192.168.50.50"
+SERVER_PORT = 2021
+MUNDO_PORT = 0
+TURTLE_SIZE = 20  #tamaño del jugador
+WORLD_SIZE = 100  #cuadrado de 100 x 100
+
+from socket import *
+import threading as th
+import logging as log
+from pynput import keyboard
+import turtle
+import xwing,random
+
+
+angleMap = {'N':90,'E':0,'S':270,'W':180}
+quitFlag = False
+you = []
+friends = {}
+def updateScreen(screen,firstMundoCnd):
+    firstMundoCnd.wait()
+    global you
+    global friends
+    tartarugas = {}
+    tartarugasColor = {}
+    oldfriends = {}
+    yout = turtle.Turtle(shape="xwing",visible=False)
+    yout.color("white")
+    yout.resizemode("user")
+    yout.turtlesize(2,2,2)
+    yout.penup()
+    yout.goto((you[0]/(WORLD_SIZE/2))*(screen.window_width()/2-TURTLE_SIZE/2), (you[1]/(WORLD_SIZE/2))*(screen.window_height()/2-TURTLE_SIZE/2))
+    yout.tiltangle(angleMap.get(you[2]))
+    yout.pendown()
+    yout.showturtle()
+    turtle.tracer(0,0)
+    while True:
+        f1=friends.copy()
+        s = set(oldfriends) ^ set(f1)
+        for x in s:
+            if x in f1:
+                tartarugas[x]= turtle.Turtle(shape="xwing",visible=False)
+                tartarugas[x].resizemode("user")
+                tartarugas[x].turtlesize(2,2,2)
+                if x in tartarugasColor:
+                    tartarugas[x].color('#%02x%02x%02x' % (int(tartarugasColor[x][0][0]*255),int(tartarugasColor[x][0][1]*255),int(tartarugasColor[x][0][2]*255)))
+                else:
+                    tartarugas[x].color('#%02x%02x%02x' % (random.randrange(40,255,1),random.randrange(40,255,1),random.randrange(40,255,1)))
+                tartarugasColor[x] = tartarugas[x].color()
+                tartarugas[x].penup()
+                tartarugas[x].goto((f1[x][0]/(WORLD_SIZE/2))*(screen.window_width()/2-TURTLE_SIZE/2), (f1[x][1]/(WORLD_SIZE/2))*(screen.window_height()/2-TURTLE_SIZE/2))
+                tartarugas[x].tiltangle(angleMap.get(f1[x][2]))
+                tartarugas[x].pendown()
+                tartarugas[x].showturtle()
+            else:
+                tartarugas[x].hideturtle()
+                del tartarugas[x]
+        oldfriends = f1
+        for x,t in tartarugas.items():
+            t.penup()
+            t.goto((oldfriends[x][0]/(WORLD_SIZE/2))*(screen.window_width()/2-TURTLE_SIZE/2), (oldfriends[x][1]/(WORLD_SIZE/2))*(screen.window_height()/2-TURTLE_SIZE/2))
+            t.tiltangle(angleMap.get(oldfriends[x][2]))
+            t.pendown()
+        yout.penup()
+        yout.goto((you[0]/(WORLD_SIZE/2))*(screen.window_width()/2-TURTLE_SIZE/2), (you[1]/(WORLD_SIZE/2))*(screen.window_height()/2-TURTLE_SIZE/2))
+        yout.tiltangle(angleMap.get(you[2]))
+        yout.pendown()
+        turtle.update()
+        if quitFlag:
+            return
+
+def readWorld(clientSkt,firstMundoCnd):
+    global you
+    global friends
+    tic = 0
+    while True:
+        msg,cli = clientSkt.recvfrom(4048)
+        msg = msg.decode().split("\n")
+        msg = msg[:-1]
+        log.debug("World data received: "+str(msg))
+        for x in msg:
+            msg0 = x.split(" ")
+            if(msg0[0]=="WORLD"):
+                if float(float(msg0[1])<float(tic)):
+                    log.warning("Received outdated mundo package. Ignoring...")
+                    break
+                elif float(float(msg0[1])>tic):
+                    friends.clear()
+                    tic = float(msg0[1])
+            elif(msg0[0]=="PLAYER"):
+                you = [float(msg0[1]),float(msg0[2]),msg0[3]]
+            else:
+                friends[msg0[0]] = [float(msg0[1]),float(msg0[2]),msg0[3]]
+        firstMundoCnd.set()
+        if quitFlag:
+            clientSkt.close()
+            return
+
+
+
+
+def controlCliente(nombre,retVal: list):
+    log.info("Starting control client...")
+    try:
+        clientSkt = socket(AF_INET,SOCK_STREAM)
+    except Exception as e :
+        log.fatal("Failed to create socket: socket."+str(e))
+        return
+    try:
+        clientSkt.connect((SERVER_HOSTNAME,SERVER_PORT))
+    except Exception as e:
+        log.fatal("Failed to connect to control server: "+str(e))
+
+    msg =  "PLAYER " + nombre + "\n"
+    msg_bytes = msg.encode()
+    msg_size = len(msg_bytes)
+    bytesSent = 0
+    while(msg_size!=bytesSent):
+        try:
+            sent = clientSkt.send(msg_bytes[bytesSent:])
+        except Exception as e:
+            log.fatal("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        bytesSent += sent
+    object = ""
+    while (object.find("\n")==-1):
+        try:
+            data = clientSkt.recv(4096).decode()
+        except Exception as e:
+            log.fatal("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        object += data
+    log.debug("OK RECV: "+object)
+    msg0 = object.split("\n")[0]
+    msg0 = msg0.split(" ")
+    if(msg0[0] != "OK"):
+        log.fatal("Server rejected connection: "+msg0[0]+" "+msg0[1])
+        clientSkt.close()
+        return
+    try:
+        mundoSkt = socket(AF_INET,SOCK_DGRAM)
+        mundoSkt.bind(('',0))
+    except Exception as e :
+        log.error("Failed to initialize mundo client socket: "+str(e))
+    mundoSktPort = mundoSkt.getsockname()
+    mundoSktPort = mundoSktPort[1]
+    msg =  "LISTEN " + str(mundoSktPort) + "\n"
+    msg_bytes = msg.encode()
+    msg_size = len(msg_bytes)
+    bytesSent = 0
+    while(msg_size!=bytesSent):
+        try:
+            sent = clientSkt.send(msg_bytes[bytesSent:])
+        except Exception as e:
+            log.fatal("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        bytesSent += sent  
+    log.debug("MSG SENT: "+msg)
+    object = ""
+    while (object.find("\n")==-1):
+        try:
+            data = clientSkt.recv(4096).decode()
+        except Exception as e:
+            log.warning("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        object += data
+    if(object!="OK\n"):
+        log.warning("Control connection closed. Mundo Listen failure")
+        clientSkt.close()
+        return
+    retVal.append(clientSkt)
+    retVal.append(mundoSkt)
+    return
+
+def keyCommand(key,clientSkt):
+    global quitFlag
+    msg = ""
+    try:
+        if (key.char == "w" or key==keyboard.Key.up):
+            msg = "GO N\n" 
+        elif (key.char == "a" or key==keyboard.Key.left):
+            msg = "GO W\n" 
+        elif (key.char == "d" or key==keyboard.Key.right):
+            msg = "GO E\n" 
+        elif (key.char == "s" or key==keyboard.Key.down):
+            msg = "GO S\n"
+        elif (key.char == "q" or key==keyboard.Key.esc):
+            log.info("Control connection closed by keypress. Shutting down gracefully...")
+            quitFlag = True
+            clientSkt.close()
+            return
+    except:
+        if (key==keyboard.Key.up):
+            msg = "GO N\n" 
+        elif (key==keyboard.Key.left):
+            msg = "GO W\n" 
+        elif (key==keyboard.Key.right):
+            msg = "GO E\n" 
+        elif (key==keyboard.Key.down):
+            msg = "GO S\n"
+        elif (key==keyboard.Key.esc):
+            log.info("Control connection closed by keypress. Shutting down gracefully...")
+            quitFlag = True
+            clientSkt.close()
+            return
+    msg_bytes = msg.encode()
+    msg_size = len(msg_bytes)
+    bytesSent = 0
+    while(msg_size!=bytesSent):
+        try:
+            sent = clientSkt.send(msg_bytes[bytesSent:])
+        except Exception as e:
+            log.fatal("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        bytesSent += sent  
+    if(msg_size>0):
+        log.info("New command: "+msg)
+
+def main():
+    username = input("Ingrese su username: ")
+    log.basicConfig(format='%(levelname)s- %(message)s')
+    log.root.setLevel(log.INFO)
+    retVal = []
+    ctrlCliente = th.Thread(target=controlCliente,args=(username,retVal))
+    ctrlCliente.start()
+    ctrlCliente.join()
+    if(len(retVal)!=2):
+        log.fatal("Control connection failed. Quitting...")
+        return -1
+    log.info("Control connection successful. Listening keyboard")
+    listener = keyboard.Listener(on_press=lambda event: keyCommand(event,retVal[0]))
+    listener.start()
+    firstMundoCnd = th.Event()
+    readWorldth = th.Thread(target=readWorld,args=(retVal[1],firstMundoCnd))
+    readWorldth.start()
+
+    screen = turtle.Screen()
+    screen.setup(1000,1000)
+    screen.addshape("xwing",xwing.Xwing)
+    screen.bgpic("bg.gif")
+    updateScreen(screen,firstMundoCnd)
+    listener.stop()
+    readWorldth.join()
+    screen.bye()
+    return 0
+
+if __name__ == "__main__":
+    main()
---
+++ servidor.py
@@ -0,0 +1,296 @@
+######### X-Wing flight game server #########
+
+############ SERVER CONFIGURATION ###########
+CONTROL_PORT = 2021
+SPEED_CONTROL = 5
+DELTATIME_CONTROL = 0.01
+VISUAL_RANGE = 15
+MAX_CONNECTIONS_PER_HOST = 2000
+DESIREDMAX_UDP_MSGSIZE = 400
+MAX_USERNAME_LEN = 300
+#############################################
+
+
+import _thread as thread
+import queue
+import random,time,re,numpy
+from socket import *
+import logging as log
+import threading as th
+import math
+
+### Mundo = {username: [hostname,port,x,y,direction]}
+Mundo = {}
+Broadcasters = {}
+MundoLock = thread.allocate_lock()
+
+def botChk(clientHost,clientPort):
+    i=0
+    for x,y in Mundo.items():
+        if y[0]==clientHost and y[1]==clientPort:
+            i+=1
+            if i>MAX_CONNECTIONS_PER_HOST:
+                return False
+    return True
+def usernameChk(username):
+    return not (username in Mundo)
+def mundoPortChk(clientHost,clientPort):
+    for x,y in Mundo.items():
+        if y[0]==clientHost and y[1]==clientPort:
+            return False
+    return True
+
+def mundoBroadcaster(username,clientHost,clientPort):
+    global Broadcasters
+    try:
+        serverSkt = socket(AF_INET,SOCK_DGRAM)
+    except Exception as e :
+        log.error("Failed to initialize mundoBroadcast socket: "+str(e))
+    while True:
+        M = Mundo #Solo lectura
+        you = M[username]
+        friends = []
+        for key,value in M.items():
+            if key!=username and math.sqrt((you[2]-value[2])**2 + (you[3]-value[3])**2)<=VISUAL_RANGE:
+                friends.append([key,value])
+        msgHead = "WORLD "+str(time.time())+"\nPLAYER "+str(you[2])+" "+str(you[3])+" "+str(you[4])+"\n"
+        msgHead = msgHead.encode()
+        msg=""
+        i=0
+        for f in friends:
+            msg += str(f[0])+" "+str(f[1][2])+" "+str(f[1][3])+" "+str(f[1][4])+"\n"
+            msgb = msg.encode()
+            msgbl = len(msgHead)+len(msgb)
+            i+=1
+            if(msgbl>=DESIREDMAX_UDP_MSGSIZE or i==len(friends)):
+                bsent=0
+                msgb=msgHead+msgb
+                while(msgbl!=bsent):
+                    try:
+                        b=serverSkt.sendto(msgb[bsent:],(clientHost,clientPort))
+                    except Exception as e:
+                        log.error("Brodcaster for "+username+" has failed: "+str(e))
+                        serverSkt.close()
+                        Broadcasters[username]=-1
+                        return
+                    bsent += b
+                msg=""
+        if len(friends)==0:
+            msgb = msgHead
+            msgbl = len(msgb)
+            bsent=0
+            while(msgbl!=bsent):
+                try:
+                    b=serverSkt.sendto(msgb[bsent:],(clientHost,clientPort))
+                except Exception as e:
+                    log.error("Brodcaster for "+username+" has failed: "+str(e))
+                    serverSkt.close()
+                    Broadcasters[username]=-1
+                    return
+                bsent += b
+        if Broadcasters[username]==0:
+            log.info("Stopped broadcaster for "+username+".")
+            serverSkt.close()
+            return
+        time.sleep(DELTATIME_CONTROL)
+
+def procesarComandos(commandBuffer: queue.Queue,username,clientHost,clientPort):
+    MundoLock.acquire()
+    while(not commandBuffer.empty()):
+        c = commandBuffer.get()
+        new_direction = c.split(" ")[1]
+        Mundo[username][4]=new_direction
+    MundoLock.release()
+
+def crearUsuario(username,hostname,port):
+    MundoLock.acquire()
+    Mundo[username] = [hostname,port,random.randrange(-40,40,1),random.randrange(-40,40,1),random.choice(["N","S","E","W"])]
+    MundoLock.release()
+
+def atenderCliente(clientSkt: socket):
+    global Broadcasters
+    log.info("New control connection on: "+str(clientSkt.getpeername()))
+    object = ""
+    while (object.find("\n")==-1):
+        try:
+            data = clientSkt.recv(4096).decode()
+        except Exception as e:
+            log.warning("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        object += data
+    msg0 = object.split("\n")[0] #no debería ser necesario
+    msg0 = msg0.split(" ")
+    clientHost,clientPort = clientSkt.getpeername()
+    msg = ""
+    if(msg0[0]!="PLAYER"):
+        msg = "FAIL BAD_FORMAT\n"
+    elif(not botChk(clientHost,clientPort)):
+        msg = "FAIL BOT_BLOCKED\n"
+    elif(not usernameChk(msg0[1])):
+        msg = "FAIL USERNAME_IN_USE\n"
+    elif(len(msg0[1].encode())>MAX_USERNAME_LEN):
+        msg = "FAIL USERNAME_TOOLARGE\n"
+    else:
+        msg = "OK\n"
+    msg_bytes = msg.encode()
+    msg_size = len(msg_bytes)
+    bytesSent = 0
+    while(msg_size!=bytesSent):
+        try:
+            sent = clientSkt.send(msg_bytes[bytesSent:])
+        except Exception as e:
+            log.warning("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        bytesSent += sent
+    if(msg.split(" ")[0]=="FAIL"):
+        log.info("Connection rejected: "+msg)
+        clientSkt.close()
+        return
+    log.info("Control answer for @"+msg0[1]+": "+msg[:-1])
+    objectPort = ""
+    while (objectPort.find("\n")==-1):
+        try:
+            data = clientSkt.recv(4096).decode()
+        except Exception as e:
+            log.warning("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        objectPort += data
+    clientPort = objectPort.split("\n")[0].split(" ")[1]
+    clientPort = int(clientPort)
+    log.info("@"+msg0[1]+" requested broadcast at port "+str(clientPort))
+    if(not mundoPortChk(clientHost,clientPort)):
+        msg="FAIL MUNDO_PORT_IN_USE\n"
+    else:
+        msg = "OK\n"
+    log.info("Control listen answer: "+msg[:-1])
+    msg_bytes = msg.encode()
+    msg_size = len(msg_bytes)
+    bytesSent = 0
+    while(msg_size!=bytesSent):
+        try:
+            sent = clientSkt.send(msg_bytes[bytesSent:])
+        except Exception as e:
+            log.warning("Control connection closed. "+str(e))
+            clientSkt.close()
+            return
+        bytesSent += sent
+    if(msg == "FAIL MUNDO_PORT_IN_USE\n"):
+        log.warning("Control connection closed. ")
+        clientSkt.close()
+        return
+    crearUsuario(msg0[1],clientHost,clientPort)
+    log.info("A new player has joined: "+msg0[1]+" on "+clientHost+":"+str(clientPort))
+    commandBuffer = queue.Queue()
+    Broadcasters[msg0[1]] = 1
+    mundoBroadcast = th.Thread(target=mundoBroadcaster,args=(msg0[1],clientHost,clientPort))
+    mundoBroadcast.start()
+    while True:
+        while (object.find("\n")==-1):
+            try:
+                data = clientSkt.recv(4096).decode() #ERROR?
+            except Exception as e:
+                log.info("Control connection closed. "+str(e))
+                clientSkt.close()
+                Broadcasters[msg0[1]] = 0
+                mundoBroadcast.join()
+                log.info("Control connection for "+msg0[1]+" ended gracefully.")
+                MundoLock.acquire()
+                Mundo.pop(msg0[1])
+                MundoLock.release()
+                return
+            if data == "":
+                log.info("Control connection closed. EOF received")
+                clientSkt.close()
+                Broadcasters[msg0[1]] = 0
+                mundoBroadcast.join()
+                log.info("Control connection for "+msg0[1]+" ended gracefully.")
+                MundoLock.acquire()
+                Mundo.pop(msg0[1])
+                MundoLock.release()
+                return                
+            object += data
+        comandos = object.split("\n")
+        for x in comandos[:-1]:
+            if re.match(r"^GO\s[NSEW]$", x):
+                commandBuffer.put(x)
+        if comandos[-1] != "":
+            object = comandos[-1]
+        else:
+            object = ""
+        procesarComandos(commandBuffer,msg0[1],clientHost,clientPort)
+        if Broadcasters[msg0[1]] == -1:
+            clientSkt.close()
+            log.info("Control connection for "+msg0[1]+" ended gracefully.")
+            MundoLock.acquire()
+            Mundo.pop(msg0[1])
+            MundoLock.release()
+            return
+
+def controlListener():
+    log.info("Starting control server...")
+    try:
+        serverSkt = socket(AF_INET,SOCK_STREAM)
+        serverSkt.bind(('',CONTROL_PORT))
+    except Exception as e :
+            log.root.fatal("Failed to create socket: socket."+str(e))
+            return
+    serverSkt.listen(5)
+    log.info("Control server initialized.")
+    while(True):
+            clientSkt, addr = serverSkt.accept()
+            th.Thread(target=atenderCliente,args=(clientSkt,)).start()
+    serverSkt.close()
+    return
+
+#SIMULADOR DEL MUNDO
+def mundoSimulator():
+    while True:
+        MundoLock.acquire()
+        for user,v in Mundo.items():
+            if v[4]=="N":
+                v[3] += SPEED_CONTROL*DELTATIME_CONTROL
+            elif v[4]=="S":
+                v[3] -= SPEED_CONTROL*DELTATIME_CONTROL
+            elif v[4]=="E":
+                v[2] += SPEED_CONTROL*DELTATIME_CONTROL
+            elif v[4]=="W":
+                v[2] -= SPEED_CONTROL*DELTATIME_CONTROL
+            v[3]=numpy.clip(v[3],-50,50)
+            v[2]=numpy.clip(v[2],-50,50)
+        MundoLock.release()
+        time.sleep(DELTATIME_CONTROL)
+        log.debug("Mundo="+str(Mundo))
+
+
+def main():
+    print("""
+                                --)-----------|____________|
+                                              ,'       ,'
+                -)------========            ,'  ____ ,'
+                         `.    `.         ,'  ,'__ ,'
+                           `.    `.     ,'       ,'
+                             `.    `._,'_______,'__
+                               [._ _| ^--      || |
+                       ____,...-----|__________ll_|\\
+      ,.,..-------\"\"\"\"\"     \"----'                 ||
+  .-""  |=========================== ______________ |
+   "-...l_______________________    |  |'      || |_]
+                                [`-.|__________ll_|      X-Wing Flight Server
+                              ,'    ,' `.        `.      By Grupo06
+                            ,'    ,'     `.    ____`.    Curso Redes de computadoras FING-UDELAR
+                -)---------========        `.  `.____`.
+                                             `.        `.
+    By Grupo06                                 `.________`.
+                              --)-------------|___________|
+    """)
+    log.basicConfig(format='%(levelname)s- %(message)s')
+    log.root.setLevel(log.INFO)
+    th.Thread(target=controlListener).start()
+    th.Thread(target=mundoSimulator).start()
+
+if __name__ == "__main__":
+    main()
+
